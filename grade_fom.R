2481cc88d0d5ec55476bf274af722bfaf813e474
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-16 13:26:19 -0300

    Recortar a área das Florestas Ombrófilas Mistas para o Sul do Brasil

--- grade_fom.R
+++ grade_fom.R
@@ -35,3 +35,18 @@ ggplot() +
   geom_sf(data = br, color = "black") +
   geom_sf(data = fom, color = "forestgreen", fill = "forestgreen",
           alpha = 0.3)
+
+# Grade ----
+
+## Recortando a FOM apenas para o Sul ----
+
+fom_recortada <- fom |>
+  sf::st_join(br |> dplyr::filter(name_region == "Sul")) |>
+  dplyr::filter(!name_region |> is.na())
+
+fom_recortada
+
+ggplot() +
+  geom_sf(data = br, color = "black") +
+  geom_sf(data = fom_recortada, color = "forestgreen", fill = "forestgreen",
+          alpha = 0.3)
