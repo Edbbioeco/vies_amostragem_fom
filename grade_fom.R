cbf085b186d8a1d9f2cdc580c3aa5d6927a309f8
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-16 13:36:25 -0300

    Gerar e visualizar a grade

--- grade_fom.R
+++ grade_fom.R
@@ -50,3 +50,21 @@ ggplot() +
   geom_sf(data = br, color = "black") +
   geom_sf(data = fom_recortada, color = "forestgreen", fill = "forestgreen",
           alpha = 0.3)
+
+# Grade ----
+
+## Gerar a grade -----
+
+grade <- fom_recortada |>
+  sf::st_make_grid(cellsize = (10 / 111.3194)) |>
+  sf::st_sf() |>
+  sf::st_join(fom_recortada) |>
+  dplyr::filter(!id1 |> is.na()) |>
+  dplyr::mutate(ID = paste0("c", dplyr::row_number()))
+
+grade
+
+ggplot() +
+  geom_sf(data = fom_recortada, color = "forestgreen", fill = "forestgreen",
+          alpha = 0.3) +
+  geom_sf(data = grade, color = "darkred", fill = NA)
