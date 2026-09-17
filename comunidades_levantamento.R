e11cf6e20d559420752afb75834db016436f83f1
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-17 08:13:55 -0300

    Corrigir taxonomia e criar variável de Ordem e Família

--- comunidades_levantamento.R
+++ comunidades_levantamento.R
@@ -101,3 +101,137 @@ df_id_fom <- coord_sf_fom |>
   dplyr::select(Local, ID)
 
 df_id_fom
+
+## Fazer o join para os dados de registro ----
+
+sps_id <- sps_trat |>
+  dplyr::left_join(df_id_fom,
+                   by = "Local") |>
+  dplyr::select(-c(geometry, Local)) |>
+  dplyr::mutate(
+    Especies = trimws(Especies),
+    Especies = dplyr::case_match(
+      Especies,
+      "Chelonoidis carbonaria" ~ "Chelonoidis carbonarius",
+      "Uromacerina ricardinii" ~ "Cercophis auratus",
+      "Philodryas aestivus" ~ "Philodryas aestiva",
+      "Placosoma glabelum" ~ "Placosoma glabellum",
+      "Bothrops alternatus neuw." ~ "Bothrops alternatus",
+      "Ecpleopus gaudichaudi" ~ "Ecpleopus gaudichaudii",
+      "Micrurus silvae" ~"Micrurus silviae",
+      "Xenodon merremi" ~ "Xenodon merremii",
+      "Rachidelus brazili" ~ "Rhachidelus brazili",
+      "Paraphimophis rustica" ~ "Paraphimophis rusticus",
+      "Oxyrophus rhombifer" ~ "Oxyrhopus rhombifer",
+      "Philodryas olfersi" ~ "Philodryas olfersii",
+      "Leposternon microcephalum" ~ "Leposternon microcephalus",
+      "Tomodon dorsatum" ~ "Tomodon dorsatus",
+      "Tupinambis merianae" ~ "Salvator marianae",
+      "Mabuya frenata" ~ "Notomabuya frenata",
+      c("Anisiolepis grilli", "Anisolepis grilli")  ~ "Urostrophus grilli",
+      "Mabuya dorsivittata" ~ "Aspronema dorsivittatum",
+      "Sibynomorphus neuwiedi" ~ "Dipsas neuwiedi",
+      "Liotyphlops beui" ~ "Liotyphlops ternetzii",
+      "Amphisbaena darwini trachura" ~ "Amphisbaena darwinii",
+      "Pantodactylus schreibersii" ~ "Cercosaura schreibersii",
+      c("Bothrops neuwiedi diorus",
+        "Bothrops newwiedi") ~ "Bothrops neuwiedi",
+      "Mastigodryas bifossatus" ~ "Palusophis bifossatus",
+      "Liophis miliaris" ~ "Erythrolamprus miliaris",
+      "Sibynomorphus mikanii" ~ "Dipsas mikanii",
+      "Liophis jaegeri" ~ "Erythrolamprus jaegeri",
+      "Phalotris iheringii" ~ "Phalotris lemniscatus",
+      "Thamnodynastes hypoconia" ~ "Dryophylax hypoconia",
+      "Thamnodynastes strigatus" ~ "Mesotes strigatus",
+      "Atractus taeniatus" ~ "Atractus paraguayensis",
+      "Crotalus durissus terrificus" ~ "Crotalus durissus",
+      "Echinanthera affinis" ~ "Dibernardia affinis",
+      "Bothrops neuwiedi" ~ "Bothrops neuwiedii",
+      "Amphisbaena darwini" ~ "Amphisbaena darwinii",
+      "Anops kingii" ~ "Amphisbaena kingii",
+      "Amphisbaena mertensi" ~ "Amphisbaena mertensii",
+      c("Varanus salvator",
+        "Tupinambis teguixin",
+        "Tupinambis teguixim",
+        "Salvator marianae") ~ "Salvator merianae",
+      "Bothrops trigemina" ~ "Bothrops alternatus",
+      "Bothrops neuwiedi paranaensis" ~ "Bothrops pubescens",
+      c("Anolis philopunctatus",
+        "Lygophis lineatus",
+        "Dipsas indica",
+        "Clelia plúmbea",
+        "Xenodon biligonigerus") ~ NA_character_,
+      .default = Especies
+    ),
+    Especies = Especies |> str_replace("Sibynomorphus", "Dipsas"),
+    Family = dplyr::case_when(
+      Especies |>
+        stringr::str_detect("Acanthochelys|Hydromedusa|Phrynops") ~ "Chelidae",
+      Especies |>
+        stringr::str_detect("Trachemys") ~ "Emydidae",
+      Especies |>
+        stringr::str_detect("Amerotyphlops") ~ "Typhlopidae",
+      Especies |>
+        stringr::str_detect("Liotyphlops") ~ "Anomalepididae",
+      Especies |>
+        stringr::str_detect("Amphisbaena|Leposternon") ~ "Amphisbaenidae",
+      Especies |>
+        stringr::str_detect("Hemidactylus") ~ "Gekkonidae",
+      Especies |>
+        stringr::str_detect("Contomastix|Salvator|Teius oculatus") ~ "Teiidae",
+      Especies |>
+        stringr::str_detect("Cercosaura|Colobodactylus|Mesotes|Pantodactylus|Placosoma") ~ "Gymnophthalmidae",
+      Especies |>
+        stringr::str_detect("Notomabuya") ~ "Scincidae",
+      Especies |>
+        stringr::str_detect("Diploglossus|Ophiodes") ~ "Diploglossidae",
+      Especies |>
+        stringr::str_detect("Enyalius|Urostrophus") ~ "Leiosauridae",
+      Especies |>
+        stringr::str_detect("Tropidurus") ~ "Tropiduridae",
+      Especies |>
+        stringr::str_detect("Epicrates|Eunectes") ~ "Boidae",
+      Especies |>
+        stringr::str_detect("Bothrops|Crotalus") ~ "Viperidae",
+      Especies |>
+        stringr::str_detect("Micrurus") ~ "Elapidae",
+      Especies |>
+        stringr::str_detect("Chironius|Leptophis|Spilotess|Tropidodryas|Palusophis|Spilotes") ~ "Colubridae",
+      .default = "Dipsadidae"
+    ),
+    Family = dplyr::case_when(
+      Especies |>
+        stringr::str_detect("Enyalius") ~ "Leiosauridae",
+      Especies |>
+        stringr::str_detect("Liotyphlops") ~ "Anomalepididae",
+      Especies |>
+        stringr::str_detect("Notomabuya") ~ "Scincidae",
+      Especies |>
+        stringr::str_detect("Ophiodes") ~ "Diploglossidae",
+      Especies |>
+        stringr::str_detect("Podocnemis") ~ "Podocnemididae",
+      Especies |>
+        str_detect(
+          "Apostolepis|Atractus|Boiruna|Clelia|Dibernardia|Dipsas|Dryophylax|Echinanthera|Erythrolamprus|Gomesophis|Oxyrhopus|Helicops|Imantodes|Mesotes|Paraphimophis|Phalotris|Philodryas|Pseudoboa|Ptychophis|Rhachidelus|Siphlophis|Taeniophallus|Thamnodynastes|Tomodon|Tropidodryas|Cercophis|Xenodon|Lygophis") ~ "Dipsadidae",
+      Family == "Varanidae" ~ "Teiidae",
+      Family|> stringr::str_detect("Xenodon") ~ "Dipsadidae",
+      Family |> stringr::str_detect("inae") ~ Family |>
+        stringr::str_replace("inae", "idae"),,
+      .default = Family
+    ),
+    Especies = Especies |> stringr::str_trim(),
+    Ordem = dplyr::case_when(
+
+      Family %in% c("Chelidae",
+                    "Emydidae",
+                    "Podocnemididae",
+                    "Testudinidae") ~ "Testudines",
+      .default = "Squamata"
+      )
+  ) |>
+  dplyr::relocate(c(ID, Family), .before = 1) |>
+  dplyr::filter(!ID |> is.na())
+
+sps_id
+
+sps_id |> dplyr::glimpse()
