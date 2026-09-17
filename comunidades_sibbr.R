144c134f58b31c258d4c6f07ef53c5b84360d573
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-17 08:54:17 -0300

    Tratr os registros de ocorrÊncias de espécies

--- comunidades_sibbr.R
+++ comunidades_sibbr.R
@@ -32,3 +32,41 @@ sibbr <- readr::read_csv2("sibbr.csv", quote = ";")
 sibbr
 
 sibbr |>  dplyr::glimpse()
+
+### Tratar ----
+
+sibbr_trat <- sibbr |>
+  dplyr::select(c(order, family, Species,
+                  decimalLongitude:decimalLatitude)) |>
+  dplyr::rename("Order" = order,
+                "Family" = family) |>
+  dplyr::filter(!Order |> is.na() &
+                  !decimalLatitude |> is.na() &
+                  !decimalLongitude |> is.na()) |>
+  dplyr::mutate(decimalLongitude = decimalLongitude  |>
+                  stringr::str_replace(
+                    "^(-?\\d{2})(\\d+)$", "\\1.\\2") |>
+                  as.numeric(),
+                decimalLongitude = dplyr::case_when(
+                  decimalLongitude >= 0 ~ decimalLongitude * -1,
+                  .default = decimalLongitude),
+                decimalLatitude = case_when(stringr::str_detect(
+                  as.character(decimalLatitude),
+                  "^(-?[1-2])") ~ str_replace(
+                    as.character(decimalLatitude),
+                    "^(-?\\d{2})(\\d+)$", "\\1.\\2"),
+                  stringr::str_detect(
+                    as.character(decimalLatitude),
+                    "^(-?[3-9])") ~ stringr::str_replace(
+                      as.character(decimalLatitude),
+                      "^(-?\\d{1})(\\d+)$", "\\1.\\2"),
+                  TRUE ~ as.character(decimalLatitude)) |>
+                  as.numeric(),
+                decimalLatitude = dplyr::case_when(
+                  decimalLatitude >= 0 ~ decimalLatitude * -1,
+                  .default = decimalLatitude)) |>
+  tidyr::drop_na()
+
+sibbr_trat
+
+sibbr_trat |>  dplyr::glimpse()
