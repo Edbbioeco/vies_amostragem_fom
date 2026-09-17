a9a7b293ef4188e3f4f698ac78cfc3227948a68f
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-17 09:03:19 -0300

    Criar data frame organizado dos registros de ocorrências

--- comunidades_sibbr.R
+++ comunidades_sibbr.R
@@ -99,3 +99,15 @@ sibbr_sf_fom
 ggplot() +
   geom_sf(data = grade) +
   geom_sf(data = sibbr_sf_fom)
+
+# Data frame dos registtros ----
+
+## Criar o data frame ----
+
+registros_sibbr <- sibbr_sf_fom |>
+  dplyr::mutate(decimalLongitude = sf::st_coordinates(geometry)[, 1],
+                decimalLatitude = sf::st_coordinates(geometry)[, 2]) |>
+  as.data.frame() |>
+  dplyr::select(-geometry)
+
+registros_sibbr
