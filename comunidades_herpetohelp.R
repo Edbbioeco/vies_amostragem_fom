2da60d0e56b5c5ce6a6daca16a54da134dfcf62a
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-18 11:03:38 -0300

    Importar registros de ocorrências das espécies

--- comunidades_herpetohelp.R
+++ comunidades_herpetohelp.R
@@ -22,3 +22,10 @@ grade
 
 ggplot() +
   geom_sf(data = grade)
+
+## Registros de ocorrência ----
+
+### Importar ----
+
+herpetohelp <- readxl::read_xlsx("herptohelp.xlsx",,
+                                 sheet = 2)
