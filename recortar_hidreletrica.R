5d13cbf868b1041f53840711cef0399ad5af74c6
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-19 13:59:45 -0300

    Importar os shapefiles das hidrelétricas como um só

--- recortar_hidreletrica.R
+++ recortar_hidreletrica.R
@@ -16,3 +16,21 @@ grade
 
 ggplot() +
   geom_sf(data = grade)
+
+# Hidrelétricas ----
+
+## Importar ----
+
+hid <- purrr::map_dfr(
+  c("Centrais_Geradoras",
+    "Pequenas_Centrais",
+    "Usinas_Hidrelétricas"),
+  \(shp){
+
+    arquivo <- list.files(pattern = paste0("^", shp, ".*\\.shp$"))
+
+    sf::st_read(arquivo) |>
+      dplyr::mutate(tipo = shp, .before = 1)
+
+  },
+  .progress = TRUE)
