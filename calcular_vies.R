8f29db5ac244d95b4970c8e19174a2b53a830fab
Edbbioeco <edsonbbiologia@gmail.com>
2026-09-20 00:51:24 -0300

    Lista de pacotes do script

---
+++ calcular_vies.R
@@ -0,0 +1,23 @@
+# PAcotes ----
+
+library(readxl)
+
+library(tidyverse)
+
+library(sf)
+
+library(sampbias)
+
+library(performance)
+
+library(ggview)
+
+library(segmented)
+
+library(flextable)
+
+library(terra)
+
+library(tidyterra)
+
+library(spdep)
